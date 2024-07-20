4cd0f9e7b200b8e78deb352de481546dc2043de5
unh6unh6 <ruddbs3308@naver.com>
2024-07-20 20:41:38 +0900

    optical flow HS example
    optical flow HS example with ocean drone video

---
+++ opticalFlow_test.m
@@ -0,0 +1,19 @@
+clearvars;
+vidReader = VideoReader('test.mp4','CurrentTime',11);
+opticFlow = opticalFlowHS
+
+h = figure;
+movegui(h);
+hViewPanel = uipanel(h,'Position',[0 0 1 1],'Title','Plot of Optical Flow Vectors');
+hPlot = axes(hViewPanel);
+
+while hasFrame(vidReader)
+    frameRGB = readFrame(vidReader);
+    frameGray = im2gray(frameRGB);  
+    flow = estimateFlow(opticFlow,frameGray);
+    imshow(frameRGB)
+    hold on
+    plot(flow,'DecimationFactor',[5 5],'ScaleFactor',60,'Parent',hPlot);
+    hold off
+    pause(10^-3)
+end
